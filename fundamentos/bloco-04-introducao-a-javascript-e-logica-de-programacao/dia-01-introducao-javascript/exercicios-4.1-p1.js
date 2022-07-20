fc69205ed224fbf291fb447bd85d76ab4ff6374b
Yanni <yannidilly.prog@gmail.com>
2022-07-20 16:05:29 -0300

    finalizei exercicios p1 do dia 4.1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/exercicios-4.1-p1.js
@@ -0,0 +1,13 @@
+const myName = "Yanni";
+const birthCity = "Juiz de Fora, MG - Brasil"
+let birthYear = 2001;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "São Paulo";
+console.log(birthCity);
